86071bc8f703d72459485bc1d01ff9aee34d349e
Mobola Falomo <mobolafalomo@gmail.com>
2023-01-03 17:24:06 -0800

    python loops

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	"""Check for multiples of 3 and 5 and replace with fizzbuzz"""
-	for x in range(1,101):
-		if x % 3 == 0 and x % 5 == 0:
-			print("fizzBuzz ", end="")
-		elif x % 3 == 0:
-			print("Fizz ", end="")
-		elif x % 5 == 0:
-			print("Buzz ", end="")
-		else:
-	 		print("{}".format(x),end=" ")
-fizzbuzz()
+    """Check for multiples of 3 and 5 and replace with fizzbuzz"""
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("fizzBuzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{}".format(x), end=" ")
