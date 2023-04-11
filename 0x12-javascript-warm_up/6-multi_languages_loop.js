06d08e4fbdf3150a6fcd0ab2a81cbfb13d454d31
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-11 16:27:48 -0700

    warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
+// A script that prints 3 lines
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i=0; i < lines.length; i++) {
+for (let i = 0; i < lines.length; i++) {
   console.log(`${lines[i]}`);
 }
