f9a585301cb45884e07c0cbe3ff2dca2cff5420b
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-11 15:59:05 -0700

    warm up

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-
+//A script that prints a constant variable 'myVar' with the value 'JavaScript is amazing'
 const myVar = 'JavaScript is amazing';
-  console.log(myVar);
+console.log(myVar);
