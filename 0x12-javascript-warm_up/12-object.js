9d42666c0227b295521d17468ee246ba19fcadbc
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-11 17:16:12 -0700

    warm up

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -4,7 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-//the 'value' property of 'myObject' was updated to 89
-myObject.value = 89; 
+//  the 'value' property of 'myObject' was updated to 89
+myObject.value = 89;
 
 console.log(myObject);
