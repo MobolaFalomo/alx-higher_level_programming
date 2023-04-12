412b5f1dc1c9b5428f79a71a65dda62c20d03945
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-11 17:01:07 -0700

    warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -8,9 +8,5 @@ if (isNaN(size)) {
 }
 
 for (let i = 0; i < size; i++) {
-  let row = '';
-  for (let j = 0; j < size; j++) {
-    row += 'x';
-  }
-  console.log(row);
+  console.log('X'.repeat(size));
 }
