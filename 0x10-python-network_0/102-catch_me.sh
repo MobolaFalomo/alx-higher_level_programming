fc09780e9cb469acc77e07fca349c4f4b90964d4
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-27 18:54:57 -0700

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message in the body of the response
-curl -s -XPUT -d "user_id=123" "0.0.0.0:5000/catch_me" -H "Referer: 0.0.0.0:5000/catch_me" > /dev/null
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
