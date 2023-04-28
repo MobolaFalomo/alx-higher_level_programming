cec685b99aeddd759e9456e56ef8cd2c215471a7
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-28 13:16:52 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A Python script that fetchesa URL """
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        web_content = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(web_content)}")
+        print(f"\t- content: {web_content}")
+        print(f"\t- utf8 content: {web_content.decode('utf-8')}")
